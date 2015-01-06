546fae19e10ccb027ab0a9baee29864bb37d38e6
Andrew <andyjacuw@gmail.com>
2015-01-06 15:39:06 -0800

    Add unshelf method to Book

--- library.js
+++ library.js
@@ -27,15 +27,30 @@ var nextShelfId = (function() {
 
 Book.prototype.enshelf = function(shelf) {
   if (this.onShelf === shelf.id) {
-    console.log('It looks like the book with that particular id is already on this shelf.\n');
+    console.log('It looks like that particular book is already on this shelf.\n');
   }
   else if (this.onShelf !== undefined && this.onShelf !== shelf.id) {
-    console.log(['It looks like the book with that particular id is already on shelf ', this.onShelf, '.'].join(''));
+    console.log(['It looks like that particular book is already on shelf ', this.onShelf, '.'].join(''));
     console.log('It will need to be removed before you can put it on this shelf.\n');
   }
   else {
-    this.onShelf = shelf.id;
     shelf.books[this.id] = { title: this.title, author: this.author };
+    this.onShelf = shelf.id;
     console.log(['A copy of ', '\'', this.title, '\'', ' with an id of (', this.id, ') has been put on shelf ', shelf.id, '.\n'].join(''));
+  }
+};
+
+Book.prototype.unshelf = function(shelf) {
+  if (this.onShelf === shelf.id) {
+    console.log(['A copy of ', '\'', this.title, '\'', ' with an id of (', this.id, ') has been removed from shelf ', shelf.id, '.\n'].join(''));
+    delete this.onShelf;
+    delete shelf.books[this.id];
+  }
+  else if (this.onShelf !== undefined && this.onShelf !== shelf.id) {
+    console.log(['It looks like that particular book is actually on shelf ', this.onShelf, '.'].join(''));
+    console.log('You will need to head over to that shelf if you want to remove it.\n');
+  }
+  else {
+    console.log('It looks like that book isn\'t on any shelf.\nYou\'ll need to put it on a shelf first, if you want to remove it\n');
   }
 };
