6998505e11d87cbee4368e6dfb97d10fc90c3b3b
Andrew <andyjacuw@gmail.com>
2015-01-11 20:50:43 -0800

    Fix style of if/elses

--- book.js
+++ book.js
@@ -19,12 +19,10 @@ var nextBookId = (function() {
 Book.prototype.enshelf = function(shelf) {
   if (this.shelf === shelf) {
     console.log('It looks like that particular book is already on this shelf.\n');
-  }
-  else if (this.shelf !== undefined) {
+  } else if (this.shelf !== undefined) {
     console.log('It looks like that particular book is already on shelf ' + this.shelf.id + '.');
     console.log('It will need to be removed before you can put it on this shelf.\n');
-  }
-  else {
+  } else {
     this.shelf = shelf;
     shelf.books[this.id] = this;
     console.log(['A copy of ', '\'', this.title, '\'', ' with an id of (', this.id, ') has been put on shelf ', shelf.id, '.\n'].join(''));
@@ -36,8 +34,7 @@ Book.prototype.unshelf = function() {
     console.log(['A copy of ', '\'', this.title, '\'', ' with an id of (', this.id, ') has been removed from shelf ', this.shelf.id, '.\n'].join(''));
     this.shelf.removeBook(this.id);
     this.shelf = undefined;
-  }
-  else {
+  } else {
     throw new Error('Tried to unshelve an unshelved book with id ' + this.id);
   }
 };
