c247af94610afac216dadbd0d5da645f1f5bf1af
Andrew <andyjacuw@gmail.com>
2015-01-12 01:23:37 -0800

    Remove test code

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
 var Library = require('./library');
 var Shelf = require('./shelf');
 var Book = require('./book');
-
-var library1 = new Library();
-var shelf1 = new Shelf();
-var book1 = new Book('Harry Potter', 'J.K. Rowling');
